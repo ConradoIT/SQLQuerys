21139fea29f1b6eb5a7f0fdf09c5f74cae78b95b
Conrado Basso <stallosdev@outlook.com>
2019-07-29 08:39:01 -0300

    adicionado querie de conexoes abertas

---
+++ SQL_PERFORMACE/conexoes_abertas.sql
@@ -0,0 +1,14 @@
+SELECT   
+    c.session_id as Id_Sessao, 
+	c.net_transport as Protocolo,
+    s.open_transaction_count as TransacoesAbertas, 
+	s.status as StatusConexao, 
+	s.host_name as NomeHost, 
+	s.program_name as NomePrograma,   
+    s.login_name as NomeUsuario, 
+	s.nt_domain as Dominio,   
+    s.nt_user_name as NomeUsuarioRede, 
+	c.connect_time as TempoDeConexao,   
+    s.login_time as TempodeLogin   
+FROM sys.dm_exec_connections AS c  
+JOIN sys.dm_exec_sessions AS s ON c.session_id = s.session_id  
